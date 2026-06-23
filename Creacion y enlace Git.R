9564407e047c8c2e06376c8e39afd33e8c0ae03c
nunez.micaela-soc <nunez.micaela@pucp.edu.pe>
2026-06-23 01:25:04 -0500

    No me di cuenta que exporte la base de datos en un script que no le corresponde.

--- Creacion y enlace Git.R
+++ Creacion y enlace Git.R
@@ -53,6 +53,3 @@ git push -u origin main
 usethis::create_github_token()
 gitcreds::gitcreds_set()
 
-#4. Exportamos base de datos creada--------------------------------------
-install.packages("arrow")
-renv::snapshot()
